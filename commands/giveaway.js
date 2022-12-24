73ae4ead0f811753eecdf8bb8757fd5e2f6cb62a
tiballion <timothee.ballion.33@gmail.com>
2022-12-25 00:15:28 +0100

    can now create giveaways

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,5 +1,27 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const createEmbed = (question) => {
+	const embed = new MessageEmbed()
+		.setColor('#0099ff')
+		.setTitle('Giveaway')
+		.setDescription(question)
+		.setTimestamp()
+		.setFooter('Giveaway');
+	return embed;
+}
+
+const createGiveaway = (channel) => {
+	channel.send('Giveaway!');
+}
+
+const checkGiveawayChannel = (interaction) => {
+	const guild = interaction.guild;
+	const channels = guild.channels.cache;
+	const giveawayChannel = channels.find(channel => channel.name === 'giveaways');
+	if(giveawayChannel) createGiveaway(giveawayChannel);
+	return giveawayChannel;
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('giveaway')
@@ -8,6 +30,8 @@ module.exports = {
 	async execute(interaction) {
 		const question = interaction.options.getString('text');
 		const sender = interaction.user;
-		await interaction.reply(`The giveaway is ${question}!`);
+		const giveaway = checkGiveawayChannel(interaction);
+		const reponse = giveaway ? `The giveaway is ${giveaway}!` : `There is no giveaway channel!`;
+		await interaction.reply(reponse);
 	},
 };
