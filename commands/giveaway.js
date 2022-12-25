c2d1b251f4a8c4827f4f079529f37ea000750022
tiballion <timothee.ballion.33@gmail.com>
2022-12-25 16:03:05 +0100

    added timestamp to embed

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,23 +1,27 @@
 const {  SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const SECONDS = 1000;
 
 
-const createEmbed = () => {
+const createEmbed = (title) => {
 	const embed = new EmbedBuilder()
-		.setTitle('Giveaway')
+		.setTitle(title)
 		.setDescription('React to this message to enter the giveaway!')
 		.setColor('#FF0000')
+		.setTimestamp(Date.now());
 	return embed;
 }
 
-const createGiveaway = (channel) => {
-	channel.send({ embeds: [createEmbed()] });
+const createGiveaway = (channel, title) => {
+	channel.send({ embeds: [createEmbed(title)] }).then((message) => {
+		message.react('🎉');
+	})
 }
 
 const checkGiveawayChannel = (interaction) => {
 	const guild = interaction.guild;
 	const channels = guild.channels.cache;
 	const giveawayChannel = channels.find(channel => channel.name === 'giveaways');
-	if(giveawayChannel) createGiveaway(giveawayChannel);
+	if(giveawayChannel) createGiveaway(giveawayChannel, interaction.options.getString('title'));
 	return giveawayChannel;
 }
 
@@ -25,9 +29,9 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('giveaway')
 		.setDescription('Create a giveaway!')
-		.addStringOption(option => option.setName('text').setDescription('the text')),
+		.addStringOption(option => option.setName('title').setDescription('The title of the giveaway!')),
 	async execute(interaction) {
-		const question = interaction.options.getString('text');
+		const title = interaction.options.getString('title');
 		const sender = interaction.user;
 		const giveaway = checkGiveawayChannel(interaction);
 		const reponse = giveaway ? `The giveaway is ${giveaway}!` : `There is no giveaway channel!`;
